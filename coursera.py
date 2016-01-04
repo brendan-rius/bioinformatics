0e6d5ef26b0da23a10878c2871c05a10719810b7
Brendan Rius <brendan@omixy.com>
2016-01-04 09:48:44 +0100

    Add coursera middleman to convert coursera parameters to usable parameters

---
+++ coursera.py
@@ -0,0 +1,6 @@
+def to_profile_matrix(text):
+    """
+    Convert a profile matrix from coursera (string) to a usable profile matrix
+    """
+    matrix = [[float(n) for n in row.split()] for row in text.split('\n')]
+    return list(zip(*matrix))  # We transpose the matrix
