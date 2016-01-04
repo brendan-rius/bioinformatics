b0a53771b8cfa98c1fd4d003779da0552c6f4dc1
Brendan Rius <brendan@omixy.com>
2016-01-04 08:21:51 +0100

    Merge all motifs related functions into a single file

--- motifs.py
+++ motifs.py
@@ -1,5 +1,8 @@
 import math
 
+from kmer import kmers
+from neighbors import neighbors
+
 
 def motif_entropy(motifs):
     """
@@ -53,7 +56,37 @@ def motif_entropy(motifs):
     return sum(entropy(distribution(v)) for v in transposed)
 
 
+def motif_enumeration(sequences, k, d):
+    """
+    Check if a motif of length k appears in each sequence in strings with at most d mismatches
+    :param sequences: the array of sequences
+    :param k: the length of the motif
+    :param d: the maximum number of mismatches
+    :return: the (k, d)-motifs in string as a set
+    """
+    motifs = set()
+    for kmer in kmers(sequences, k):
+        neighborhood = neighbors(kmer, d)
+        for neighbor in neighborhood:
+            neighborhood2 = neighbors(neighbor, d)
+            if all(any(neighbor2 in seq for neighbor2 in neighborhood2) for seq in sequences):
+                motifs.add(neighbor)
+    return motifs
+
+
 def __main__():
+    sequences = """CAATTTACGGAGCCGTCTGATGTTT
+CCGACAGGCGCTACCCCTGAGACGT
+TTCCCTTCCATCTGAGAAACGCGCA
+ACTGACAAATCAGCCAAGTCTCTAG
+TTGGCGCAAGAAGAATCTGACATAT
+TCTTAGTTTTCCAGGTTTCCCCTGA""".split('\n')
+    k = 5
+    d = 1
+    print(' '.join(list(motif_enumeration(sequences, k, d))))
+
+
+def __main2__():
     motifs = ['TCGGGGgTTTtt',
               'cCGGtGAcTTaC',
               'aCGGGGATTTtC',
